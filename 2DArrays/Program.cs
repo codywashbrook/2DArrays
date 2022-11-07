bcf5facc98d6d9921d5cef12bfe229adbe3bb246
codywashbrook <codywashbrook@hotmail.com>
2022-11-07 11:34:00 -0400

    Update Program.cs

--- 2DArrays/Program.cs
+++ 2DArrays/Program.cs
@@ -10,6 +10,43 @@ namespace _2DArrays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("2D Arrays/Multi-Dimensional");
+            Console.WriteLine();
+
+            //single value
+            int score = 100;
+
+            //array
+            int[] ammo = new int[4];
+            ammo[0] = 1;
+            ammo[1] = 20;
+            ammo[2] = 35;
+            ammo[3] = 4;
+
+            //2D array of 2D map
+            int[,] map = new int[20, 10]; //200 values //loop for the x, y, loop x from 0 - 19, loop y 
+            map[0, 0] = 0;
+            map[1, 0] = 0;
+            map[2, 0] = 0;
+
+            //,,
+            map[19, 9] = 0;
+            
+            // coint from 1..10
+            int i = 1; //start
+            while (i <= 10) // end
+            {
+                Console.WriteLine(i);
+                i = i + 1; // increment
+            }
+            i = 1; //start // reset after og i finsished
+            while (i <= 10) // end
+            {
+                Console.WriteLine(i);
+                i = i + 1; // increment
+            }
+            Console.WriteLine();
+            Console.ReadKey(true);
         }
     }
 }
